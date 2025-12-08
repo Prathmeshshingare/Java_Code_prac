0ec65c7068dd6c3c00684d6afc2b6796580144b1
Prathmeshshingare <prathmeshshingare4@gmail.com>
2025-12-08 23:39:48 +0530

    EvenoddProgram

---
+++ src/com/basic/DemoEvenOdd.java
@@ -0,0 +1,18 @@
+package com.basic;
+
+public class DemoEvenOdd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int num=10;
+		if(num % 2 == 0)
+		{
+			System.out.println(num+":Number is Even");
+		}
+		else
+			System.out.println(num+":Number is Odd");
+
+	}
+
+}
