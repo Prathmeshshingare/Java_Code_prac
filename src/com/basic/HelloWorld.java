59730a51e939cdb27d7943bcb5aad0185d2a9fd6
Prathmeshshingare <prathmeshshingare4@gmail.com>
2025-12-08 23:32:05 +0530

    commit first

---
+++ src/com/basic/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.basic;
+
+public class HelloWorld {
+	
+	public void Display() {
+		System.out.println("Hello World....!");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		HelloWorld hw=new HelloWorld();
+		hw.Display();
+
+	}
+
+}
