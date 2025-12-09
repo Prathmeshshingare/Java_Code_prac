0ab7cebe9ad7f5684dd418aec124d53aa6032894
Prathmeshshingare <prathmeshshingare4@gmail.com>
2025-12-09 10:12:35 +0530

    fibonacci pro added

---
+++ src/com/basic/DmoFibonacci.java
@@ -0,0 +1,26 @@
+package com.basic;
+
+public class DmoFibonacci {
+	
+	public int fibonacciSeries(int num) {
+		int f=0;
+		int s=1;
+		int fib=0;
+		for(int i=2;i<=num;i++) {
+			fib=f+s;
+			f=s;
+			s=fib;
+		
+		System.out.println(fib);
+		}
+		return fib;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		DmoFibonacci df=new DmoFibonacci();
+		df.fibonacciSeries(10);
+
+	}
+
+}
