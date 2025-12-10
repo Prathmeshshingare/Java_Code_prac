7d2acce8305bfa2ded563f0da628a29c313d9123
Prathmeshshingare <prathmeshshingare4@gmail.com>
2025-12-10 22:20:49 +0530

    swap way first

--- src/com/basic/NumberToBinary.java
+++ src/com/basic/NumberToBinary.java
@@ -4,17 +4,17 @@ public class NumberToBinary {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int num=17;
-		int[] arr=new int[1000];
-		int i=0;
+		int num = 17;
+		int[] arr = new int[1000];
+		int i = 0;
 		while (num > 0) {
-			arr[i]=num % 2;
-			num=num /2;
-			i++;	
+			arr[i] = num % 2;
+			num = num / 2;
+			i++;
 		}
- for(int j=i-1;j >=0;j--) {
-	 System.out.println(arr[j]);
- }
+		for (int j = i - 1; j >= 0; j--) {
+			System.out.println(arr[j]);
+		}
 	}
 
 }
---
+++ src/com/basic/SwapeOfTwoNumbers.java
@@ -0,0 +1,20 @@
+package com.basic;
+
+public class SwapeOfTwoNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10;
+		int b=20;
+		System.out.println("value of a before swap: "+a);
+		System.out.println("value of b before swap: "+b);
+		int temp=a;
+		a=b;
+		b=temp;
+		
+		System.out.println("value of a after swap: "+a);
+		System.out.println("value of b after swap: "+b);
+
+	}
+
+}
