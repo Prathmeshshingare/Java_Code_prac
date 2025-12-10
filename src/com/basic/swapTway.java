6389a50c94104e17dfd44290d0326e95dc0f8847
Prathmeshshingare <prathmeshshingare4@gmail.com>
2025-12-10 22:35:37 +0530

    swap third way

---
+++ src/com/basic/swapTway.java
@@ -0,0 +1,21 @@
+package com.basic;
+
+public class swapTway {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10;
+		int b=20;
+	
+		System.out.println("value of a before swap"+a);
+		System.out.println("value of b before swap"+b);
+		
+		a=a ^ b;
+		b=a ^ b;
+		a=a ^b;
+		System.out.println("value of a after swap"+a);
+		System.out.println("value of b after swap"+b);
+
+	}
+
+}
