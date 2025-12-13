550cf55e0bc50d5ca4eb2473e2a57b1a17f486e3
Prathmeshshingare <prathmeshshingare4@gmail.com>
2025-12-13 19:57:05 +0530

    iterate elements

---
+++ src/com/arrayq/PrintArrElets.java
@@ -0,0 +1,14 @@
+package com.arrayq;
+
+public class PrintArrElets {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int [] arr= {5,4,6,7,34,65,78,32,11};
+		for(int i=0;i<=arr.length-1;i++) {
+			System.out.println(arr[i]);
+		}
+
+	}
+
+}
