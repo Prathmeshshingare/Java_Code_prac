694172b4fe0779ec46d9b5240bcd8d411ba62a97
Prathmeshshingare <prathmeshshingare4@gmail.com>
2025-12-13 20:10:25 +0530

    added

---
+++ src/com/arrayq/ReverseArray.java
@@ -0,0 +1,15 @@
+package com.arrayq;
+
+public class ReverseArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int arr[]= {9,8,7,6,5,4,3};
+		for(int i=arr.length-1;i>=0;i--) {
+			System.out.println(arr[i]);
+		}
+
+	}
+
+}
