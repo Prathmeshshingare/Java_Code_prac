42e6e4bff62cc03fe824d19b969523124f4a6346
Prathmeshshingare <prathmeshshingare4@gmail.com>
2025-12-13 20:13:36 +0530

    sum of elets

---
+++ src/com/arrayq/SumOfElements.java
@@ -0,0 +1,16 @@
+package com.arrayq;
+
+public class SumOfElements {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int arr[]= {10,15,20,30};
+		int sum=0;
+		for(int i=0;i<=arr.length-1;i++) {
+			sum=sum+arr[i];
+			
+		}
+       System.out.println("Sum of Elements:"+sum);
+	}
+
+}
