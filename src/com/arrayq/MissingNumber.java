fd23674008bc75116947988172a72ad55e670949
Prathmeshshingare <prathmeshshingare4@gmail.com>
2025-12-15 20:36:53 +0530

    missing number

---
+++ src/com/arrayq/MissingNumber.java
@@ -0,0 +1,23 @@
+package com.arrayq;
+
+public class MissingNumber {
+
+    public static int findMissing(int[] arr, int n) {
+
+        int expectedSum = n * (n + 1) / 2;
+        int actualSum = 0;
+
+        for (int i : arr) {
+            actualSum += i;
+        }
+
+        return expectedSum - actualSum;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {1, 2, 4, 5};
+        int n = 5;
+
+        System.out.println(findMissing(arr, n));
+    }
+}
