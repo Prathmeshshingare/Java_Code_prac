bc87f1fe71a857419598f78d291e0002aacfeaaf
Prathmeshshingare <prathmeshshingare4@gmail.com>
2025-12-16 22:36:49 +0530

    freqency of number

---
+++ src/com/arrayq/FreqencyOfElements.java
@@ -0,0 +1,34 @@
+package com.arrayq;
+
+public class FreqencyOfElements {
+	
+	void feqOfElement(int arr[]) {
+
+		for(int i=0;i<=arr.length-1;i++) {
+			
+			if(arr[i]==-1) {
+				continue;
+			}
+			int c=1;
+			for(int j=i+1;j<=arr.length-1;j++) {
+				if(arr[i] ==arr[j]) {
+					c++;
+					arr[j]=-1;
+				}
+			}
+		
+				System.out.println(arr[i]+" element count "+c);
+			
+		}
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		FreqencyOfElements fe=new FreqencyOfElements();
+		int arr[]= {1,2,2,3,4,5,6,6,6};
+		fe.feqOfElement(arr);
+
+	}
+
+}
