eff03fb3f4ff6ec6f96eb9fa05ee1e4fc652932e
Prathmeshshingare <prathmeshshingare4@gmail.com>
2025-12-18 23:03:39 +0530

    updated

--- src/com/arrayq/SumPair.java
+++ src/com/arrayq/SumPair.java
@@ -1,10 +1,8 @@
 package com.arrayq;
 
 public class SumPair {
-
+//sum of two elements equal to sum
 	public static void main(String[] args) {
-	
-		
 		int arr[]= {2,3,4,5,6,7};
 		int sum=9;
 		for(int i=1;i<=arr.length-1;i++) {
