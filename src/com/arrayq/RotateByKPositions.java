6204b475a460932495f8cbb4f02428c28f366b4a
Prathmeshshingare <prathmeshshingare4@gmail.com>
2025-12-18 22:30:45 +0530

    rotate by k

--- src/com/arrayq/RotateByKPositions.java
+++ src/com/arrayq/RotateByKPositions.java
@@ -1,14 +1,28 @@
 package com.arrayq;
-
 public class RotateByKPositions {
-	
-	void rotateByKPosistion(int arr[],int k) {
-		
-	}
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    static void rotateByK(int[] arr, int k) {
 
-	}
+        int n = arr.length;
 
+        for (int t = 0; t < k; t++) {
+
+            int last = arr[n - 1];
+
+            for (int i = n - 1; i > 0; i--) {
+                arr[i] = arr[i - 1];
+            }
+
+            arr[0] = last;
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {1, 2, 3, 4, 5};
+        rotateByK(arr, 2);
+
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+    }
 }
