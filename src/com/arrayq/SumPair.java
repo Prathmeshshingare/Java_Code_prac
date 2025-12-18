4a97ec2fae9ddcacffbe52614e7591513b716597
Prathmeshshingare <prathmeshshingare4@gmail.com>
2025-12-18 22:39:28 +0530

    sum pair

---
+++ src/com/arrayq/SumPair.java
@@ -0,0 +1,20 @@
+package com.arrayq;
+
+public class SumPair {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int arr[]= {2,3,4,5,6,7};
+		int sum=9;
+		for(int i=1;i<=arr.length-1;i++) {
+			for(int j=1;j<=arr.length-1;j++) {
+				if((arr[i] + arr[j]) == sum) {
+					System.out.println(arr[i] +" : "+arr[j]);
+				}
+			}
+		}
+
+	}
+
+}
