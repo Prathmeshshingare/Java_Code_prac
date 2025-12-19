97d3f520f8bfac41fc42247c505b2a5c3b00d702
Prathmeshshingare <prathmeshshingare4@gmail.com>
2025-12-19 23:28:13 +0530

    updated

--- src/com/stringq/SentenceToWordArray.java
+++ src/com/stringq/SentenceToWordArray.java
@@ -5,9 +5,9 @@ public class SentenceToWordArray {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String s="Java is Best Lang";
-		String[] sc=s.split(" ");
+		String[] word=s.split(" ");
 		
-		for(String ss:sc) {
+		for(String ss:word) {
 			System.out.println(ss+" ");
 		}
 
