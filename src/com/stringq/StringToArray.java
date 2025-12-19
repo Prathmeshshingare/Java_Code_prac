c2cedb491dba38c9aac10938662d61920c173891
Prathmeshshingare <prathmeshshingare4@gmail.com>
2025-12-19 23:13:23 +0530

    string to char array

---
+++ src/com/stringq/StringToArray.java
@@ -0,0 +1,16 @@
+package com.stringq;
+
+public class StringToArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s="vijay";
+		
+		char[] ch=s.toCharArray();
+		
+		for(char c:ch) {
+			System.out.println(c);
+		}
+	}
+
+}
