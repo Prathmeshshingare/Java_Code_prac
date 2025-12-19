2aa1ff331b0f9908f11fdbbc4fa985a8ab9de367
Prathmeshshingare <prathmeshshingare4@gmail.com>
2025-12-19 23:26:31 +0530

    .

---
+++ src/com/stringq/SentenceToWordArray.java
@@ -0,0 +1,16 @@
+package com.stringq;
+
+public class SentenceToWordArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s="Java is Best Lang";
+		String[] sc=s.split(" ");
+		
+		for(String ss:sc) {
+			System.out.println(ss+" ");
+		}
+
+	}
+
+}
