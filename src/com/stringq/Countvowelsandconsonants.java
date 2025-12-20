6339c1529ee26dcf45cffa73eca6d90b13f13508
Prathmeshshingare <prathmeshshingare4@gmail.com>
2025-12-20 22:07:15 +0530

    vowel count

---
+++ src/com/stringq/Countvowelsandconsonants.java
@@ -0,0 +1,20 @@
+package com.stringq;
+
+public class Countvowelsandconsonants {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str="education";
+		int vowel=0;
+		int cons=0;
+		for(int i=0;i<str.length();i++) {
+	
+			if(str.charAt(i) == 'a' || str.charAt(i) =='e' ||str.charAt(i) == 'i' || str.charAt(i) =='o' ||str.charAt(i) =='u') {
+				vowel++;
+			}
+		cons++;
+		}
+     System.out.println(vowel);
+	}
+
+}
