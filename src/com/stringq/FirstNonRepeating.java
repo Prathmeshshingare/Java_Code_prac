479f9592c764081f324e8ffd43dc6410ae31cc41
Prathmeshshingare <prathmeshshingare4@gmail.com>
2025-12-21 23:27:56 +0530

    FirstNonRepeating

---
+++ src/com/stringq/FirstNonRepeating.java
@@ -0,0 +1,28 @@
+package com.stringq;
+
+public class FirstNonRepeating {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String str="sawiss";
+		for(int i=0;i<str.length();i++) {
+			
+			char ch=str.charAt(i);
+			int count=0;
+			for(int j=0;j<str.length();j++) {
+				
+				if(str.charAt(j)==ch) {
+					count++;
+				}
+			}
+			
+			if(count == 1) {
+				System.out.println(ch);
+				return;
+			}
+		}
+
+	}
+
+}
