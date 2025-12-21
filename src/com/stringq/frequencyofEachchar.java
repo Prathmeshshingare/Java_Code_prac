4a5176831fbefc44fb3d616251f2aa6a4ec6d6ee
Prathmeshshingare <prathmeshshingare4@gmail.com>
2025-12-21 10:53:07 +0530

    updated

--- src/com/stringq/frequencyofEachchar.java
+++ src/com/stringq/frequencyofEachchar.java
@@ -2,29 +2,28 @@ package com.stringq;
 
 public class frequencyofEachchar {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        String str = "pprogramm";
-        char[] c =str.toCharArray();
+		String str = "javaprogram";
+		char[] c = str.toCharArray();
 
-        for (int i = 0; i < c.length; i++) {
+		for (int i = 0; i < c.length; i++) {
 
-            if (c[i] == '\0') {
-                continue;
-            }
+			if (c[i] == '\0') {
+				continue;
+			}
 
-            int count = 1;
+			int count = 1;
 
-            for (int j = i + 1; j < c.length; j++) {
-                if (c[i] == c[j]) {
-                    count++;
-                    c[j] = '\0';
-                }
-            }
+			for (int j = i + 1; j < c.length; j++) {
+				if (c[i] == c[j]) {
+					count++;
+					c[j] = '\0';
+				}
+			}
 
-        
-                System.out.println(c[i] + " count is : " + count);
-      
-        }
-    }
+			System.out.println(c[i] + " count is : " + count);
+
+		}
+	}
 }
