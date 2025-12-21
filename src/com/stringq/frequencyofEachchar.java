8061bb65901289ba2757bbcc2e19df4f61ed439a
Prathmeshshingare <prathmeshshingare4@gmail.com>
2025-12-21 10:51:10 +0530

    freq of char

---
+++ src/com/stringq/frequencyofEachchar.java
@@ -0,0 +1,30 @@
+package com.stringq;
+
+public class frequencyofEachchar {
+
+    public static void main(String[] args) {
+
+        String str = "pprogramm";
+        char[] c =str.toCharArray();
+
+        for (int i = 0; i < c.length; i++) {
+
+            if (c[i] == '\0') {
+                continue;
+            }
+
+            int count = 1;
+
+            for (int j = i + 1; j < c.length; j++) {
+                if (c[i] == c[j]) {
+                    count++;
+                    c[j] = '\0';
+                }
+            }
+
+        
+                System.out.println(c[i] + " count is : " + count);
+      
+        }
+    }
+}
