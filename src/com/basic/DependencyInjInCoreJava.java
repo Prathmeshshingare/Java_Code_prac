32af6670a22e682b408665d6e5c5d5a18e58b750
Prathmeshshingare <prathmeshshingare4@gmail.com>
2025-12-24 23:52:16 +0530

    DI in core

---
+++ src/com/basic/DependencyInjInCoreJava.java
@@ -0,0 +1,68 @@
+package com.basic;
+
+public class DependencyInjInCoreJava {
+	
+	int stdid;
+	String stdname;
+	Address adr;
+	
+
+	public DependencyInjInCoreJava() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+
+	@Override
+	public String toString() {
+		return "DependencyInjInCoreJava [stdid=" + stdid + ", stdname=" + stdname + ", adr=" + adr + "]";
+	}
+
+
+	public DependencyInjInCoreJava(int stdid, String stdname, Address adr) {
+		super();
+		this.stdid = stdid;
+		this.stdname = stdname;
+		this.adr = adr;
+	}
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Address ds=new Address("mh","ind");
+		DependencyInjInCoreJava di=new DependencyInjInCoreJava(12,"vikas",ds);
+		System.out.println(di);
+
+	}
+
+}
+class Address{
+	String state;
+	String country;
+	public Address() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Address(String state, String country) {
+		super();
+		this.state = state;
+		this.country = country;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	@Override
+	public String toString() {
+		return "Address [state=" + state + ", country=" + country + "]";
+	}
+	
+}
