426c4ec8a9689e85aa410012ad5aa461bec74f0c
Prathmeshshingare <prathmeshshingare4@gmail.com>
2025-12-29 22:52:44 +0530

    first code

---
+++ src/com/InterviewAsk/Factorial.java
@@ -0,0 +1,15 @@
+package com.InterviewAsk;
+
+public class Factorial {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num=5;
+		int fact=1;
+		for(int i=num;i>1;i--) {
+			fact=fact*i;
+		}
+     System.out.println(fact);
+	}
+
+}
